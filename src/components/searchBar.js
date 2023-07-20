91e9956746f0a5acf84d76330abf0205c09b4f6b
Rafał Zakrzewski <eltharis.dev@gmail.com>
2023-07-20 17:38:47 +0200

    finish-search-logick

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
+import { TextInput, TouchableOpacity, View, StyleSheet, ActivityIndicator, Image, Text } from "react-native";
+import axios from "axios";
 
 const SearchBar = () => {
+
+    const [breedName, setBreedName] = useState("");
+    const [dogImage, setDogImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const handleSearch = async () => {
+        setError("");
+        setDogImage(null);
+        setLoading(true);
+
+        try {
+            const response = await axios.get(
+                `https://dog.ceo/api/breed/${breedName}/images/random`
+            );
+
+            const data = response.data;
+
+            if (data.status === "success") {
+                setDogImage(data.message);
+            } else {
+                setError("Breed not found. Please enter a valid dog breed name.");
+            }
+        } catch (error) {
+            setError("Error fetching the data. Please try again.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <View style={searchBarStyles.mainBarContainer}>
-            <TextInput
-                style={searchBarStyles.textInputContainer}
-                placeholder="Write dog breed name..."
-            />
-            <TouchableOpacity>
-                <View style={searchBarStyles.buttonContainer}>
-                    <Ionicons
-                        name="ios-arrow-forward"
-                        size={28}
-                        color="white"
-                    />
-                </View>
-            </TouchableOpacity>
+        <View>
+            <View style={searchBarStyles.mainBarContainer}>
+                <TextInput
+                    style={searchBarStyles.textInputContainer}
+                    placeholder="Write dog breed name..."
+                    onChangeText={(text) => setBreedName(text)}
+                />
+                <TouchableOpacity onPress={handleSearch}>
+                    <View style={searchBarStyles.buttonContainer}>
+                        <Ionicons name="ios-arrow-forward" size={28} color="white" />
+                    </View>
+                </TouchableOpacity>
+            </View>
+            {loading && <ActivityIndicator style={searchBarStyles.loader} />}
+            {error !== "" && <Text style={searchBarStyles.errorText}>{error}</Text>}
+            {dogImage && <Image source={{ uri: dogImage }} style={searchBarStyles.image} />}
         </View>
     );
 };
@@ -46,5 +79,19 @@ const searchBarStyles = StyleSheet.create({
         borderRadius: 5,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    loader: {
+        marginLeft: 10,
+        marginTop: 100,
+    },
+    errorText: {
+        color: "red",
+        marginLeft: 40,
+        marginTop: 100,
+    },
+    image: {
+        width: 320,
+        height: 300,
+        marginTop: 10,
     },
 });
